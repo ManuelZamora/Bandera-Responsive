0e87a2f68ec1ab1cc9d2cdc2befb02678c188b0f
ManuelZamora <rocker2503@gmail.com>
2021-08-05 19:52:54 -0500

    Bandera Responsive

---
+++ src/components/Bandera/Bandera.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-wrap m-auto h-96 border-2 border-black xl:flex-row-reverse lg:flex-row md:flex-row-reverse sm:flex-row-reverse">
+    <div class="item flex flex-row bg-green-700 flex-none w-1/3 "></div>
+    <div class="item flex flex-row bg-white flex-none w-1/3 "></div>
+    <div class="item flex flex-row bg-red-700 flex-none w-1/3 "></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Bandera",
+  props: {},
+};
+</script>
